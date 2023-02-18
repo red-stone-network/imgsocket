1713f3687e741194d2367379d68f51f814f07cde
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-02-18 18:18:06 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# ImgSocket by Redstone Network
+
+An easy to deploy, simple to use image hosting service template.
+
+All made using socket.io.
+
+[Try it out here.](https://imgsocket.glitch.me)
+
+###### ImgSocket is licensed under CC-BY-4.0.
---
+++ index.js
@@ -0,0 +1,59 @@
+const goodFiletypes = ["png","jpg","jpeg","jfif","gif"];
+
+const mntmode = false;
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+function genHexString(len) {
+    const hex = '0123456789ABCDEF';
+    let output = '';
+    for (let i = 0; i < len; ++i) {
+        output += hex.charAt(Math.floor(Math.random() * hex.length));
+    }
+    return output;
+}
+
+server.listen(port, function () {
+  if (mntmode == true) {
+    console.log('---IMGSOCKET - MAINTENANCE MODE---\nServer listening at port %d', port);
+  } else {
+    console.log('---IMGSOCKET---\nServer listening at port %d', port);
+  }
+});
+
+// Routing
+app.use(express.static('public'));
+
+// Chatroom
+var fs = require('fs') 
+
+io.on('connection', function (socket) {
+  socket.on('upload-image', function (data, callback) {
+    console.log(data[0]+" sent a file");
+    
+    console.log(data[1]);
+    console.log("FILENAME: "+data[2]);
+    
+    var split = data[2].split(".");
+    var random = genHexString(64);
+    console.log(random);
+    if (goodFiletypes.includes(split[1].toLowerCase()) == true) {
+      console.log("start");
+      fs.writeFile("public/images/"+random+"."+split[1], data[1], (err) => {
+        var e2 = err ? "failure" : "success"
+        console.log(e2);
+        
+        callback({
+          user: data[0],
+          message: [err, random+"."+split[1]]
+        });
+        console.log("succ")
+        console.log("end")
+      });
+    }
+  });
+});
+//lol
