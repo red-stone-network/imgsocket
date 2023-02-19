dedbfaafe1bdea2a6fa5d630853c87789bac6e22
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-02-18 21:23:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 function genHexString(len) {
-    const hex = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+~";
+    const hex = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_+~";
     let output = '';
     for (let i = 0; i < len; ++i) {
         output += hex.charAt(Math.floor(Math.random() * hex.length));
@@ -39,7 +39,7 @@ io.on('connection', function (socket) {
     console.log("FILENAME: "+data[2]);
     
     var split = data[2].split(".");
-    var random = genHexString(20);
+    var random = genHexString(9);
     console.log(random);
     if (goodFiletypes.includes(split[1].toLowerCase()) == true) {
       console.log("start");
@@ -55,7 +55,7 @@ io.on('connection', function (socket) {
         console.log("end")
       });
       var newData = "USER TOKEN: "+data[0]
-      fs.writeFile("public/images/"+random+"."+split[1]+".data", data[1], (err) => {
+      fs.writeFile("public/images/"+random+"."+split[1]+".data", newData, (err) => {
         var e2 = err ? "failure" : "success"
         console.log(e2);
         
